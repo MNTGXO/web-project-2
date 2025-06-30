c5922256d2bca3666cdf79584da8c0d6951cd9da
Musammil N <mohammedmusammiln@gmail.com>
2025-06-30 19:32:03 +0530

    Create errors.js

---
+++ utils/errors.js
@@ -0,0 +1,26 @@
+class CustomError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+class BadRequestError extends CustomError {
+  constructor(message = 'Bad Request') {
+    super(message, 400);
+  }
+}
+
+class NotFoundError extends CustomError {
+  constructor(message = 'Not Found') {
+    super(message, 404);
+  }
+}
+
+module.exports = {
+  CustomError,
+  BadRequestError,
+  NotFoundError
+};
